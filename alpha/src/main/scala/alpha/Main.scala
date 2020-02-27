707da0eee5daeca21f273a8a62ca3f12ffcbb762
Raayan Pillai <raayan@raayanpillai.com>
2020-02-27 01:32:15 -0500

    examples of adding dependencies and linking stuff up in intellij

--- alpha/src/main/scala/alpha/Main.scala
+++ alpha/src/main/scala/alpha/Main.scala
@@ -1,11 +1,14 @@
 package alpha
 
 import beta.Worker
+import org.joda.time.DateTime
 
 object Main extends App {
   println("Here we are in the program. Brought to us by ~bazel~")
 
   val worker = new Worker
+
+  println(DateTime.now())
 
   worker.swing()
 }
